84c607dabc784c51873bcffa46a050f465f5c6de
117503445 <t117503445@gmail.com>
2024-12-14 00:06:53 +0800

    unlock all
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- main.go
+++ main.go
@@ -75,27 +75,84 @@ func Process(paths ...string) {
 				break
 			}
 		}
+	}
+	for _, challenge := range []string{"chal_survivor_1", "chal_survivor_2", "chal_survivor_3", "chal_survivor_4", "chal_survivor_5", "chal_gatherer_1", "chal_gatherer_2", "chal_gatherer_3", "chal_gatherer_4", "chal_gatherer_5", "chal_difficulty_0", "chal_difficulty_1", "chal_difficulty_2", "chal_difficulty_3", "chal_difficulty_4", "chal_difficulty_5"} {
+		map_prefix_keys["challenges_completed"] = append(map_prefix_keys["challenges_completed"], challenge)
+	}
+	for _, character := range map_prefix_keys["characters_unlocked"] {
+		characterWithoutPrefix := strings.TrimPrefix(character, "character_")
+		log.Debug().Str("character", character).Str("characterWithoutPrefix", characterWithoutPrefix).Msg("Character")
 
+		map_prefix_keys["challenges_completed"] = append(map_prefix_keys["challenges_completed"], "chal_" +characterWithoutPrefix)
 	}
 
-	fileSave := "out.json"
+	difficultiesUnlocked := make([]map[string]interface{}, 0)
+	for _, character := range map_prefix_keys["characters_unlocked"] {
+		zonesDifficultyInfo := []map[string]interface{}{
+			{
+				"max_selectable_difficulty": 5,
+				"zone_id":                   0,
+				"max_difficulty_beaten": map[string]interface{}{
+					"difficulty_value": 1,
+					"enemy_damage":     1,
+					"enemy_health":     1,
+					"enemy_speed":      1,
+					"is_coop":          false,
+					"retries":          0,
+					"wave_number":      1,
+				},
+			},
+			{
+				"max_selectable_difficulty": 5,
+				"zone_id":                   1,
+				"max_difficulty_beaten": map[string]interface{}{
+					"difficulty_value": 1,
+					"enemy_damage":     1,
+					"enemy_health":     1,
+					"enemy_speed":      1,
+					"is_coop":          false,
+					"retries":          0,
+					"wave_number":      1,
+				},
+			},
+		}
+
+		difficultyUnlocked := map[string]interface{}{
+			"character_id":          character,
+			"zones_difficulty_info": zonesDifficultyInfo,
+		}
+		difficultiesUnlocked = append(difficultiesUnlocked, difficultyUnlocked)
+	}
+
+	newJSON := make(map[string]interface{})
+	for key, value := range map_prefix_keys {
+		newJSON[key] = value
+	}
+	newJSON["difficulties_unlocked"] = difficultiesUnlocked
+	newJSON["data"] = map[string]interface{}{
+		"enemies_killed":      20000,
+		"materials_collected": 20000,
+	}
+
+	fileOut := "out.json"
+	fileOld := "save_v2.json"
 
 	oldJSON := make(map[string]interface{})
-	if goutils.FileExists("save_v2.json") {
-		err := goutils.ReadJSON("save_v2.json", &oldJSON)
+	if goutils.FileExists(fileOld) {
+		err := goutils.ReadJSON(fileOld, &oldJSON)
 		if err != nil {
 			log.Fatal().Err(err).Msg("Failed to read file")
 		}
 		log.Info().Interface("oldJSON", oldJSON).Msg("Old JSON")
-		for key, value := range map_prefix_keys {
+		for key, value := range newJSON {
 			oldJSON[key] = value
 		}
-		err = goutils.WriteJSON(fileSave, oldJSON)
+		err = goutils.WriteJSON(fileOut, oldJSON)
 		if err != nil {
 			log.Fatal().Err(err).Msg("Failed to write file")
 		}
 	} else {
-		err := goutils.WriteJSON(fileSave, map_prefix_keys)
+		err := goutils.WriteJSON(fileOut, newJSON)
 		if err != nil {
 			log.Fatal().Err(err).Msg("Failed to write file")
 		}
